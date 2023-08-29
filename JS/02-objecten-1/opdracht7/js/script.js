36ad466a2d8a9f64f6d78ddf4b87bb37d5ffaf51
Mika Haukkala <mhaukkala@reacollege.eu>
2023-08-29 15:28:22 +0200

    JS 5de les (SW)

---
+++ JS/02-objecten-1/opdracht7/js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// for some reason that .innerHTML glitch keeps popping up so I keep recycling 1.6 to circumvent it..
+
+let dkc = "Donkey Kong Country";
+let anthem = "God Save the Queen";
+const lyrics1 = "I'm blue,";
+const lyrics2 = "Da ba dee da ba di";
+
+
+dkc = dkc.padEnd(30,".")
+anthem = anthem.replace("Queen", "King")
+
+document.getElementById("h-1").innerHTML = `${dkc} `
+
+document.getElementById("li-1").innerHTML = `${anthem} `
+document.getElementById("li-2").innerHTML = `${lyrics1} ${(` ${lyrics2}`).repeat(7)}`
+// document.getElementById("li-3").innerHTML = `${(  )}`
+// document.getElementById("li-4").innerHTML = `${(  )}`
