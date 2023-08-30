92d073e7ad41adebca180a2cc8b7a3e214a60192
Mika Haukkala <mhaukkala@reacollege.eu>
2023-08-30 15:24:40 +0200

    JS 6de les (SW)

---
+++ JS/03-arrays/opdracht1/js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const names = [
+    "Tom",
+    "Casper",
+    "Wieke",
+    "Bryan"
+];
+
+const movies = [
+    "movie-1",
+    "movie-2",
+    "movie-3",
+    "movie-4",
+    "movie-5"
+];
+
+movies[3] = "new-movie-6";
+document.getElementById("span-1").innerHTML = `There are ${movies.length} variables in the array`
+
+document.getElementById("li-1").innerHTML = `${names} `
+
+document.getElementById("li-2").innerHTML = `${movies[0]}`
+document.getElementById("li-3").innerHTML = `${movies[1]}`
+document.getElementById("li-4").innerHTML = `${movies[2]}`
+document.getElementById("li-5").innerHTML = `${movies[3]}`
+document.getElementById("li-6").innerHTML = `${movies[4]}`
---
+++ JS/03-arrays/opdracht2/js/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const planes =[
+	"Ixalan",
+	"Tarkir",
+	"Ravnica",
+	"Dominaria",
+	"Theros",
+	"Amonkhet",
+	"Mirrodin",
+	"Innistrad"
+];
+
+planes.unshift("Kaladesh");
+planes.splice(4, 1,);
+planes.splice(3, 0, "Lorwyn");
+planes.sort();
+planes.reverse()
+const text = planes.join();
+
+document.getElementById("span-1").innerHTML = `${planes} `
+
+document.getElementById("li-1").innerHTML = `${planes} `
+
+//document.getElementById("li-2").innerHTML = `${movies[0]}`
+//document.getElementById("li-3").innerHTML = `${movies[1]}`
+//document.getElementById("li-4").innerHTML = `${movies[2]}`
+//document.getElementById("li-5").innerHTML = `${movies[3]}`
+//document.getElementById("li-6").innerHTML = `${movies[4]}`//
