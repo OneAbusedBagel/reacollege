f78e1ea3baa1c4851fe27cb30b242e0826d84c2a
Mika Haukkala <mhaukkala@reacollege.eu>
2023-09-06 15:26:30 +0200

    JS 10de les (SW)

---
+++ JS/04-Keuzestructuren/opdracht1/js/script.js
@@ -0,0 +1,8 @@
+
+var cookies = confirm("Deze website maakt gebruik van cookies. Klik op OK om hiermee akkoord te gaan.");
+
+if (cookies == true)  {
+	document.getElementById("true").innerHTML = `U gaat akoord met cookies`;
+} else {
+	document.getElementById("false").innerHTML = `U Gaat niet akoord met cookies`;
+}
---
+++ JS/04-Keuzestructuren/opdracht2/js/script.js
@@ -0,0 +1,14 @@
+
+let rng2 = Math.round(Math.random() * 10 );
+
+if (rng2 < 5) {
+	alert("les than 5")
+} else if (rng2 = 5) {
+	alert("exactly 5")
+} else {
+	alert("greater than 5")
+}
+
+alert(rng2)
+
+console.log(rng2)
