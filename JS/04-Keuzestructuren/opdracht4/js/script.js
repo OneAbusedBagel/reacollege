f9e5af88583cd53b922bba367ac98d2178bcfaee
Mika Haukkala <mhaukkala@reacollege.eu>
2023-09-11 15:29:06 +0200

    JS 11ste les (SW)

---
+++ JS/04-Keuzestructuren/opdracht4/js/script.js
@@ -0,0 +1,21 @@
+
+var language = prompt(`maak een keuze tussen “nl”, “en”, “de” en “es”.`, `en`)
+
+switch (language) {
+	case "nl":
+		alert("Welkom");
+		break;
+	case "en":
+		alert("Welcome");
+		break;
+	case "de":
+		alert("Willkommen");
+		break;
+	case "es":
+		alert("Bienvenida");
+		break;
+	default:
+		alert("Geen taal gekozen..");
+		break;
+}
+
---
+++ JS/04-Keuzestructuren/opdracht5/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("leeftijd").onchange = function() {
+	// Plaats hier je eigen code.
+	alert(age);
+};
