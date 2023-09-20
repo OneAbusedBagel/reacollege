a639ec409d659fd0f9e189cdcd691ae16ab04280
Mika Haukkala <mhaukkala@reacollege.eu>
2023-09-20 15:26:34 +0200

    JS 15de les (SW)

--- JS/05-lussen/opdracht4/JS/script.js
+++ JS/05-lussen/opdracht4/JS/script.js
@@ -124,11 +124,47 @@ const games = [
 	},
 ];
 
+
+
+var x = 1;
+var y = 1;
+var i = 0;
+
+
 for (const element of games) {
-        console.log(element)
-    
+    console.log(element)
+		
+	document.getElementById(`art-${x}-${y}`).innerHTML = `${games[x].name}  \n`
+
+	let z = 'test'
+	switch (y) {
+		case 1:
+			z = 'name'
+			break;
+		case 2:
+			z = 'img'
+			break;
+		case 3:
+			z = 'releaseEU'
+			break;
+		case 4:
+			z = 'platform'
+			break;
+	}
+	console.log(z)
+	console.log(y)
+
+	y == 4 ? y -= 4 :
+
+	//document.getElementById(`art-${i+1}-${a+1}`).innerHTML += "<li>" + city + "</li>";
+	x++
+	y++
+	i++
 }
 
+console.log()
+console.log(y)
+console.log(i)
 
 //  let numbers = [
 
@@ -156,105 +192,3 @@ for (const element of games) {
 //  document.getElementById("true").innerHTML = `${order}`;
 
 //  alert(numbers.join(`\n`));
-
-document.getElementById("art-1-1").innerHTML = `${games[0].name} \n`
-document.getElementById("art-1-2").innerHTML = `${games[0].img} \n`
-document.getElementById("art-1-3").innerHTML = `${games[0].releaseEU} \n`
-document.getElementById("art-1-4").innerHTML = `${games[0].platform} \n`
-
-document.getElementById("art-2-1").innerHTML = `${games[1].name} \n`
-document.getElementById("art-2-2").innerHTML = `${games[1].img} \n`
-document.getElementById("art-2-3").innerHTML = `${games[1].releaseEU} \n`
-document.getElementById("art-2-4").innerHTML = `${games[1].platform} \n`
-
-document.getElementById("art-3-1").innerHTML = `${games[2].name} \n`
-document.getElementById("art-3-2").innerHTML = `${games[2].img} \n`
-document.getElementById("art-3-3").innerHTML = `${games[2].releaseEU} \n`
-document.getElementById("art-3-4").innerHTML = `${games[2].platform} \n`
-
-document.getElementById("art-4-1").innerHTML = `${games[3].name} \n`
-document.getElementById("art-4-2").innerHTML = `${games[3].img} \n`
-document.getElementById("art-4-3").innerHTML = `${games[3].releaseEU} \n`
-document.getElementById("art-4-4").innerHTML = `${games[3].platform} \n`
-
-document.getElementById("art-5-1").innerHTML = `${games[4].name} \n`
-document.getElementById("art-5-2").innerHTML = `${games[4].img} \n`
-document.getElementById("art-5-3").innerHTML = `${games[4].releaseEU} \n`
-document.getElementById("art-5-4").innerHTML = `${games[4].platform} \n`
-
-document.getElementById("art-6-1").innerHTML = `${games[5].name} \n`
-document.getElementById("art-6-2").innerHTML = `${games[5].img} \n`
-document.getElementById("art-6-3").innerHTML = `${games[5].releaseEU}`
-document.getElementById("art-6-4").innerHTML = `${games[5].platform} \n`
-
-document.getElementById("art-7-1").innerHTML = `${games[6].name} \n`
-document.getElementById("art-7-2").innerHTML = `${games[6].img} \n`
-document.getElementById("art-7-3").innerHTML = `${games[6].releaseEU} \n`
-document.getElementById("art-7-4").innerHTML = `${games[6].platform} \n`
-
-document.getElementById("art-8-1").innerHTML = `${games[7].name} \n`
-document.getElementById("art-8-2").innerHTML = `${games[7].img} \n`
-document.getElementById("art-8-3").innerHTML = `${games[7].releaseEU} \n`
-document.getElementById("art-8-4").innerHTML = `${games[7].platform} \n`
-
-document.getElementById("art-9-1").innerHTML = `${games[8].name} \n`
-document.getElementById("art-9-2").innerHTML = `${games[8].img} \n`
-document.getElementById("art-9-3").innerHTML = `${games[8].releaseEU} \n`
-document.getElementById("art-9-4").innerHTML = `${games[8].platform} \n`
-
-document.getElementById("art-10-1").innerHTML = `${games[9].name} \n`
-document.getElementById("art-10-2").innerHTML = `${games[9].img} \n`
-document.getElementById("art-10-3").innerHTML = `${games[9].releaseEU} \n`
-document.getElementById("art-10-4").innerHTML = `${games[9].platform} \n`
-
-document.getElementById("art-11-1").innerHTML = `${games[10].name} \n`
-document.getElementById("art-11-2").innerHTML = `${games[10].img} \n`
-document.getElementById("art-11-3").innerHTML = `${games[10].releaseEU} \n`
-document.getElementById("art-11-4").innerHTML = `${games[10].platform} \n`
-
-document.getElementById("art-12-1").innerHTML = `${games[11].name} \n`
-document.getElementById("art-12-2").innerHTML = `${games[11].img} \n`
-document.getElementById("art-12-3").innerHTML = `${games[11].releaseEU} \n`
-document.getElementById("art-12-4").innerHTML = `${games[11].platform} \n`
-
-document.getElementById("art-13-1").innerHTML = `${games[12].name} \n`
-document.getElementById("art-13-2").innerHTML = `${games[12].img} \n`
-document.getElementById("art-13-3").innerHTML = `${games[12].releaseEU} \n`
-document.getElementById("art-13-4").innerHTML = `${games[12].platform} \n`
-
-document.getElementById("art-14-1").innerHTML = `${games[13].name} \n`
-document.getElementById("art-14-2").innerHTML = `${games[13].img} \n`
-document.getElementById("art-14-3").innerHTML = `${games[13].releaseEU} \n`
-document.getElementById("art-14-4").innerHTML = `${games[13].platform} \n`
-
-document.getElementById("art-15-1").innerHTML = `${games[14].name} \n`
-document.getElementById("art-15-2").innerHTML = `${games[14].img} \n`
-document.getElementById("art-15-3").innerHTML = `${games[14].releaseEU} \n`
-document.getElementById("art-15-4").innerHTML = `${games[14].platform} \n`
-
-document.getElementById("art-16-1").innerHTML = `${games[15].name} \n`
-document.getElementById("art-16-2").innerHTML = `${games[15].img} \n`
-document.getElementById("art-16-3").innerHTML = `${games[15].releaseEU} \n`
-document.getElementById("art-16-4").innerHTML = `${games[15].platform} \n`
-
-document.getElementById("art-17-1").innerHTML = `${games[16].name} \n`
-document.getElementById("art-17-2").innerHTML = `${games[16].img} \n`
-document.getElementById("art-17-3").innerHTML = `${games[16].releaseEU} \n`
-document.getElementById("art-17-4").innerHTML = `${games[16].platform} \n`
-
-document.getElementById("art-18-1").innerHTML = `${games[17].name} \n`
-document.getElementById("art-18-2").innerHTML = `${games[17].img} \n`
-document.getElementById("art-18-3").innerHTML = `${games[17].releaseEU} \n`
-document.getElementById("art-18-4").innerHTML = `${games[17].platform} \n`
-
-document.getElementById("art-19-1").innerHTML = `${games[18].name} \n`
-document.getElementById("art-19-2").innerHTML = `${games[18].img} \n`
-document.getElementById("art-19-3").innerHTML = `${games[18].releaseEU} \n`
-document.getElementById("art-19-4").innerHTML = `${games[18].platform} \n`
-
-document.getElementById("art-20-1").innerHTML = `${games[19].name} \n`
-document.getElementById("art-20-2").innerHTML = `${games[19].img} \n`
-document.getElementById("art-20-3").innerHTML = `${games[19].releaseEU} \n`
-document.getElementById("art-20-4").innerHTML = `${games[19].platform} \n`
-
-
