4fb163e30dabe2c1aafabe1e02081cdeda8705f7
Mika Haukkala <mhaukkala@reacollege.eu>
2024-03-14 15:22:43 +0100

    JS 26ste les (SW)

--- JS/09-Reguliere-Expressies/opdracht2/js/script.js
+++ JS/09-Reguliere-Expressies/opdracht2/js/script.js
@@ -1,41 +1,65 @@
 "use strict"
 
-const pattern = / ^(06)\-([0-9]{8}) | ^(06 )([0-9]{8}) | ^(06)([0-9]{8}) | ^(\+31 6 ) ([0-9]{8})/
+const btn = document.querySelector("#submit")
+const invalid = document.querySelector("#invalid")
+const pattern = /^(06)\-[0-9]{8}|^(06 )([0-9]{8})|^(06)([0-9]{8})|^(\+31 6 )([0-9]{8})/
 
-console.log(pattern.test("0612345678"))
-
-const el1 = document.querySelector("#tel");
-el1.getAttribute("value");
-
-const el2 = document.getElementById("knop-1");
-el2.addEventListener("click", log())
-
-const el3 = document.getElementById("knop-2");
-el3.addEventListener("click", console.log("knop-2 werkt"));
-
-function testFunctie2() {
-    alert("testFunctie2 werkt")
-}
-
-function log() {
-    const telcheck = document.querySelector("#tel");
-    el1.getAttribute("value");
-
-    console.log(telcheck)
-
-    if (pattern.test(telcheck)) {
-        console.log(`log pass`)
+function check() {
+    let telcheck = document.querySelector("#tel").value;
+    console.log(telcheck)   
+    if (pattern.test(telcheck) == true) {
+        btn.disabled = false
+        console.log("check is true")
+        console.log(pattern)
+        console.log(telcheck)
     } else {
-        console.error(`log no pass.`)
+        btn.disabled = true
+        console.error("check sees false")
+        invalid.innerHTML = ("Invalid phone number!")
     }
-
-    alert("log function working.")
 }
+
 
 const el = document.getElementById("tel");
-el.addEventListener("input", testFunctie);
+el.addEventListener("input", check);
 
-function testFunctie() {
-    var head = document.getElementById("tel").value
-    document.getElementById("output").innerHTML = `<i> ${head} </i>`;
-}
+
+
+
+
+
+//-----------------[division line to reduce clutter]----
+
+
+// let check = "+31 6 12345678"
+
+// console.log(check)
+// console.log(pattern)
+// console.log(pattern.test(check))
+
+// function submitFunctie() {
+//     alert("don't go just yet<,!")
+// }
+
+// function log() {
+
+//     console.log(`this is the le1 log ${telcheck}`)
+
+//     if (pattern.test(telcheck) === true) {
+//         console.log(`log pass`)
+//     } else {
+//         console.error(`log no pass.`)
+//     }
+//     alert("it uh work?")
+// }
+
+// const el1 = document.getElementById("submit");
+// el1.addEventListener("click", log);
+
+// const el2 = document.getElementById("knop-1");
+// el2.addEventListener("click", log())
+
+// function testFunctie() {
+//     var head = document.getElementById("tel").value
+//     document.getElementById("output").innerHTML = `<i> ${head} </i>`;
+// }
