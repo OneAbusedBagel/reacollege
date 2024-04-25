c2d4e82d59d2600025a2a28ef8a0974f68bdfc69
OneAbusedBagel <Mikahaukkala@hotmail.com>
2024-04-25 14:46:43 +0200

    JS 40ste les (SW)

--- JavaScript/14-Fetch API/opdracht2/js/script.js
+++ JavaScript/14-Fetch API/opdracht2/js/script.js
@@ -6,10 +6,25 @@ function changeFunction() {
     fetch("imagecrawler.php")
     .then(response => response.text())
 	.then(data => {
+		console.log(data);
 		document.getElementById("output").innerHTML = data;
+
+		let stuff = document.querySelectorAll("img");
+		console.log(stuff);
+
+		for(let i = 0; i < stuff.length; i++) {
+			let alt = stuff[i].alt;
+			console.log(alt);
+			alt = alt.replace(".jpg", "");
+			alt = alt.replaceAll("-", " ");
+			alt = alt.toUpperCase();
+			stuff[i] = alt;
+		}
+		console.log(stuff)
 	})
 }
 
-setInterval(changeFunction, 30000)
+changeFunction()
+// setInterval(changeFunction, 300)
 // const form_1 = document.getElementById("versionSelect");
 // form_1.addEventListener("change", changeFunction, true);
