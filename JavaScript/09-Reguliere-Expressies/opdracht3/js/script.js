780c3b89b55778064cabecb6dd779efad1632b75
OneAbusedBagel <Mikahaukkala@hotmail.com>
2024-06-20 13:58:11 +0200

    JS 47ste les (SW)

--- JavaScript/09-Reguliere-Expressies/opdracht3/js/script.js
+++ JavaScript/09-Reguliere-Expressies/opdracht3/js/script.js
@@ -1,15 +1,7 @@
 "use strict"
 
 const names = "Branford, Terra\nCole, Locke\nGaramonde, Cyan\nChère, Celes";
-const str = "p, a, img, table, form";
 const swap = /([A-zÀ-ú]+)\,\s([A-zÀ-ú]+)/gi;
-const test = /([A-zÀ-ú]+)\,\s([A-zÀ-ú]+)/gi;
 
 console.log(names);
-console.log(names.replace(test, `$2, $1`));
-
-const newstring = names.replace(swap, "$2, $1");
-console.log(newstring);
-
-console.log(str);
-console.log(str.replace(/(\w+)/gi, "<$1>"));
+console.log(names.replace(swap, (_, lastName, firstName) => `${firstName} ${lastName.toUpperCase()}`));
--- JavaScript/09-Reguliere-Expressies/opdracht4/js/script.js
+++ JavaScript/09-Reguliere-Expressies/opdracht4/js/script.js
@@ -1,71 +1,14 @@
 "use strict"
 
-const btn = document.querySelector("#submit");
-const invalidTel = document.querySelector("#invalidTel");
-const invalidBir = document.querySelector("#invalid-bir");
-const invalidAdd = document.querySelector('#invalid-web');
-const invalidWeb = document.querySelector('#invalid-add');
-const invalidpas = document.querySelector('#invalid-pas');
+const btn = document.querySelector("#submit")
+const invalid = document.querySelector("#invalid")
+const patternTel = /^(06)\-[0-9]{8}|^(06 )([0-9]{8})|^(06)([0-9]{8})|^(\+31 6 )([0-9]{8})/
 
-const patternTel = /^(06)\-[0-9]{8}|^(06 )([0-9]{8})|^(06)([0-9]{8})|^(\+31 6 )([0-9]{8})/;
-const patternBir = / ^(*[0-9])\-(*[0-9]) | /;
-const patternAdd = / hmm? /; // straat huisnummer
-const patternWeb = / hmm? /;
-const patternpas = / hmm? /;
-
-console.log(patternTel.test("06-12345678"));
+console.log(patternTel.test("06-12345678"))
 
 function checkTel() {
-    let telCheck = document.querySelector("#tel").value;
-    console.log(telCheck)
-    if (patternTel.test(telCheck) == true) {
-        btn.disabled = false
-        console.log("check is true")
-        console.log(patternTel)
-        console.log(telCheck)
-        invalid.innerHTML = ("")
-    } else {
-        btn.disabled = true
-        console.warn("check sees false")
-        invalid.innerHTML = ("Invalid phone number!")
-    }
-};
-
-function checkBir() {
-    let telCheck = document.querySelector("#tel").value;
-    console.log(telCheck)
-    if (patternTel.test(telCheck) == true) {
-        btn.disabled = false
-        console.log("check is true")
-        console.log(patternTel)
-        console.log(telCheck)
-        invalidTel.innerHTML = ("")
-    } else {
-        btn.disabled = true
-        console.warn("check sees false")
-        invalidTel.innerHTML = ("Invalid Date Format, use: day-month")
-    }
-};
-
-function checkAdd() {
-    let addCheck = document.querySelector("#tel").value;
-    console.log(addCheck)
-    if (patternAdd.test(addCheck) == true) {
-        btn.disabled = false
-        console.log("check is true")
-        console.log(patternAdd)
-        console.log(addCheck)
-        invalidAdd.innerHTML = ("")
-    } else {
-        btn.disabled = true
-        console.warn("check sees false")
-        invalidAdd.innerHTML = ("Invalid phone number!")
-    }
-};
-
-function checkWeb() {
     let telcheck = document.querySelector("#tel").value;
-    console.log(telcheck)
+    console.log(telcheck)   
     if (patternTel.test(telcheck) == true) {
         btn.disabled = false
         console.log("check is true")
@@ -77,23 +20,7 @@ function checkWeb() {
         console.warn("check sees false")
         invalid.innerHTML = ("Invalid phone number!")
     }
-};
-
-function checkPas() {
-    let telcheck = document.querySelector("#tel").value;
-    console.log(telcheck)
-    if (patternTel.test(telcheck) == true) {
-        btn.disabled = false
-        console.log("check is true")
-        console.log(patternTel)
-        console.log(telcheck)
-        invalid.innerHTML = ("")
-    } else {
-        btn.disabled = true
-        console.warn("check sees false")
-        invalid.innerHTML = ("Invalid phone number!")
-    }
-};
+}
 
 const el = document.getElementById("tel");
 el.addEventListener("input", checkTel);
