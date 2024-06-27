82181a633393e54dad9bf66e2730bab96a29785f
OneAbusedBagel <Mikahaukkala@hotmail.com>
2024-06-27 13:55:11 +0200

    JS 49ste les (TW)

--- JavaScript/11-Objecten 2/opdracht3/js/script.js
+++ JavaScript/11-Objecten 2/opdracht3/js/script.js
@@ -1,81 +1,89 @@
 "use strict"
 
 const audi = {
-    naam: "Audi A5 2.0",
-    bouwjaar: 2009,
-    km_stand: 165000,
-    energielabel: "A",
-    brandstof: "benzine",
-    prijs:12750.00,
-    garantie: false,
-    opties: [
+    _naam: "Audi A5 2.0",
+    _bouwjaar: 2009,
+    _km_stand: 165000,
+    _energielabel: "A",
+    _brandstof: "benzine",
+    _prijs:12750,
+    _garantie: false,
+    _opties: [
         "Cruise Control",
-        "Boordcomputer",
+        "Boorcomputer",
         "Navigatiesysteem",
         "Airconditioning",
         "ABS",
     ],
+    // this stuff is broken. Ask Patrick about how to fix using a getter on a nstled object.
+    _motor: {
+        _verbruik: "15,62 km/l",
+        _motorinhoud: "1.984 cc",
+        _topsnelheid: "250 km/h",
+        _vermogen: "211 pk",
+    },
     motor: {
-        verbruik: "15,62 km/l",
-        motorinhoud: "1.984 cc",
-        topsnelheid: "250 km/h",
-        vermogen: "211 pk",
+        get verbruik() {
+            return this._verbruik
+        },
+        get motorinhoud() {
+            return this._motorinhoud
+        },
+        get topsnelheid() {
+            return this._topsnelheid
+        },
+        get vermogen() {
+            return this._vermogen
+        }
     },
     buy: function() {
-        console.log(`YOU HAVE BOUGHT THE ${this.naam} FOR ${this.prijs} WOW!! WHAT A DEAL FROM CIRCA ${this.bouwjaar}.`)
+        console.log(`YOU HAVE BOUGHT THE ${this.naam} FOR ${this.prijs} WOW!! WHAT A DEAL CIRCA ${this.bouwjaar}.`)
     },
     accelerate: function() {
-        console.log(`THIS ${this.naam} CAN GET TO A TOPSPEED OF ${this.motor.topsnelheid}. INCREDIBLE! SO FAST IT'S [almost]ILLEGAL!!`)
+        console.log(`THIS ${this.naam} CAN GET TO A TOPSPEED OF ${this.motor.topsnelheid}. INCREDIBLE! SO FAST IT'S (almost)ILLEGAL!!`)
     },
     refuel: function() {
-        console.log(`THE ${this.naam} IS NO HEAVY DRINKER ${this.motor.verbruik} SO DON'T WORRY ABOUT PAYING IT'S TAB FOR ${this.brandstof}`)
+        console.log(`THE ${this.naam} DRINKS ABOUT ${this.motor.verbruik} SO DON'T WORRY ABOUT PAYING IT'S TAB FOR ${this.brandstof}`)
     },
 
-    // get naam() {
-    //     console.log("THIS IS A RECURSION LOOP YOU IDIOT FIX IT") // I don't remember if I fixed it... | Addendum, still a recursion loop
-	// 	return this.bouwjaar+" "+this.naam;
-	// }
-
-    // useless {
-    //     // get bouwjaar() {
-    //     // 	return this.bouwjaar; 
-    //     // },
-    //     // get km_stand() {
-    //     // 	return this.km_stand; 
-    //     // },
-    //     // get energielabel() {
-    //     // 	return this.energielabel; 
-    //     // },
-    //     // get brandstof() {
-    //     // 	return this.brandstof; 
-    //     // },
-    //     // get prijs() {
-    //     // 	return "€"+this.prijs; 
-    //     // },
-    //     // get garantie() {
-    //     // 	return this.garantie; 
-    //     // },
-    //     // get opties() {
-    //     // 	return this.opties; 
-    //     // },
-    //     // get verbruik() {
-    //     // 	return this.motor.verbruik; 
-    //     // },
-    //     // get motorinhoud() {
-    //     // 	return this.motor.motorinhoud; 
-    //     // },
-    //     // get topsnelheid() {
-    //     // 	return this.motor.topsnelheid; 
-    //     // },
-    //     // get vermogen() {
-    //     // 	return this.motor.vermogen; 
-    //     // }
-    // }
+    
+    get naam() {
+        return this._naam
+    },
+    get bouwjaar() {
+        return this._bouwjaar
+    },
+    get km_stand() {
+        return this._km_stand
+    },
+    get energielabel() {
+        return this._energielabel
+    },
+    get brandstof() {
+        return this._brandstof
+    },
+    get prijs() {
+        let price = this._prijs;
+        console.log(price)
+        if(String(price).split(".")[1]?.length == 2) {
+            return ("€")+this._prijs
+        } else {
+            return ("€")+this._prijs+("00")
+        }
+    },
+    get garantie() {
+        return this._garantie ? "Ja" : "Nee";
+    },
+    get opties() {
+        return this._opties.join(", ")
+    }
 }
 
 audi.buy()
 audi.accelerate()
 audi.refuel()
+
+console.log("it all works, but man what a weird car salesman...")
 
 document.getElementById("naam").innerHTML = audi.naam
 document.getElementById("bouwjaar").innerHTML = audi.bouwjaar
@@ -103,24 +111,3 @@ for (let property in audi.motor) {
 
 document.getElementById("test2").appendChild(row)
 }
-
-
-
-// ------------------------
-
-
-// let output = "";
-// for (let property in audi.motor) {
-//     output += "<p>" + property + ": " + audi.motor[property] + "</p>";
-// }
-// document.getElementById("output").innerHTML = output
-
-
-// for (let property in audi.motor) {
-    // output += "<tr>" + "<td>" + "test 1" +"</td>" + "<td>" + "test 2" + "</td>" + "</tr>"
-    // output += "<tr>" + "<td>" + property + ":" + "</td>" + "<td>" + audi[property] + "</td>" + "<tr>";
-// }
-// document.getElementById("test2").innerHTML = output ;
-
-
-//     console.log(i)
