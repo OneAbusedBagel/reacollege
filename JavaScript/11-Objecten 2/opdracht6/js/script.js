52095747c34504578bde827d830651a2172d0466
OneAbusedBagel <Mikahaukkala@hotmail.com>
2024-07-03 15:19:44 +0200

    JS 52ste les (SW)

--- JavaScript/11-Objecten 2/opdracht6/js/script.js
+++ JavaScript/11-Objecten 2/opdracht6/js/script.js
@@ -1,20 +1,58 @@
 "use strict"
 
-let movies = []
-
-
-function Game(name, releaseDate, platform, developer) {
-    this.name = name
-    this.releaseDate = releaseDate
-    this.platform = platform
-    this.developer = developer
+function Game(name, releaseDate, platform, developer,) { 
+    Object.defineProperty(this, "name", { 
+        get: function() { 
+            return "<strong> Game: " + name + " </strong>";       
+        },        
+        set: function(newName) {            
+            name = newName;        
+        },               
+        configurable: false     
+    });
+    Object.defineProperty(this, "releaseDate", { 
+        get: function() { 
+            return "<strong> game: " + releaseDate + " </strong>";       
+        },        
+        set: function(newReleaseDate) {            
+            releaseDate = newReleaseDate;        
+        },               
+        configurable: false     
+    });
+    Object.defineProperty(this, "platform", { 
+        get: function() { 
+            return "<em> Game: " + platform + " </em>";       
+        },        
+        set: function(newPlatform) {            
+            platform = newPlatform;        
+        },               
+        configurable: false     
+    }); 
+    Object.defineProperty(this, "developer", { 
+        get: function() { 
+            return "<em> Game: " + developer + " </em>";       
+        },        
+        set: function(newDeveloper) {            
+            developer = newDeveloper;        
+        },               
+        configurable: false     
+    });
 }
 
-let suikoden = new Game("suikoden", 1997, "Playstation", "Konami")
-let suikoden2 = new Game("suikoden 2", 2000, "Playstation", "Konami")
+let suikoden = new Game("suikoden", 1997, "Playstation", "Konami",)
+let suikoden2 = new Game("suikoden 2", 2000, "Playstation", "Konami",)
 
-movies.push(vampire)
-movies.push(singham)
-movies.push(bulk)
+console.log(suikoden)
+console.log(suikoden.name)
+console.log(suikoden.releaseDate)
+console.log(suikoden.platform)
+console.log(suikoden.developer)
 
-console.log(movies)
+console.log(suikoden2.name)
+console.log(suikoden2.releaseDate)
+console.log(suikoden2.platform)
+console.log(suikoden2.developer)
+
+let html = Object.values(suikoden)
+document.getElementById("output").innerHTML = html
+
