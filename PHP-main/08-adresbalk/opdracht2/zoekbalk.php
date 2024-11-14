bc95361532ab689d08eec41fdc4189a34ba02b4b
OneAbusedBagel <Mikahaukkala@hotmail.com>
2024-11-14 15:20:42 +0100

    PHP 24ste les (SW)

--- PHP-main/08-adresbalk/opdracht2/zoekbalk.php
+++ PHP-main/08-adresbalk/opdracht2/zoekbalk.php
@@ -1,10 +1,11 @@
 <?php
-	if (isset($_POST['search-button'])){
-		if (!empty($_POST['search'])){
-			$msg = 'U wilt zoeken op: '.$_POST['search'];
+	if (isset($_GET['search-button'])){
+		if (!empty($_GET['search'])){
+			$msg = 'U wilt zoeken op: '.$_GET['search'];
 		} else {
 			$msg = 'U heeft geen geldige zoekterm opgegeven.';
 		}
+		$previous_search = $_GET['search'] ?? 'no previous search';
 	}
 ?>
 <!DOCTYPE html>
@@ -16,9 +17,9 @@
 	</head>
 	<body>
 		<section>
-			<form action="<?=htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+			<form action="<?=htmlspecialchars($_SERVER['PHP_SELF']);?>" method="GET">
 				<label for="zoeken">Zoekopdracht:</label>
-				<input type="text" name="search" id="zoeken" placeholder="Vul uw trefwoord in...">
+				<input type="text" name="search" id="zoeken" placeholder="<?= $previous_search ?? 'Vul uw trefwoord in...';?>">
 				<input type="submit" name="search-button" value="Zoeken">
 			</form>
 			<?php
