e016df0f8bc7c2396c3225f82dabfb9a9cb7254d
OneAbusedBagel <Mikahaukkala@hotmail.com>
2024-12-03 15:20:45 +0100

    PHP 28ste les (SW)

--- PHP-main/08-adresbalk/opdracht4/lib/array.php
+++ PHP-main/08-adresbalk/opdracht4/lib/array.php
@@ -1,71 +1,74 @@
 <?php
 $products = [
-    'Ni' => [
+    array(
         'release' => '23-03-2018',
         'imageSRC' => 'images/9200000053083967.jpg',
         'console' => 'PlayStation 4',
         'name' => 'Ni No Kuni II: Revenant Kingdom',
         'price' => '€ 49.99',
-    ],
-    'farcry' => [
+    ),
+    array(
         'release' => '27-03-2018',
         'imageSRC' => 'images/9200000078668488.jpg',
         'console' => 'PlayStation 4',
         'name' => 'Far Cry 5 - Deluxe Edition',
         'price' => '€ 59.99',
-    ],
-    'injustice' => [
+    ),
+    array(
         'release' => '29-03-2018',
         'imageSRC' => 'images/9200000090287767.jpg',
         'console' => 'PlayStation 4',
         'name' => 'Injustice 2 - Legendary Edition',
         'price' => '€ 59.99',
-    ],
-    'lostchild' => [
+    ),
+    array(
         'release' => '30-03-2018',
         'imageSRC' => 'images/9200000084844101.jpg',
         'console' => 'PlayStation 4',
         'name' => 'The Lost Child',
         'price' => '<del>€ 59.99</del> € 49.99',
-    ],
-    'AOT' => [
+    ),
+    array(
         'release' => '30-03-2018',
         'imageSRC' => 'images/9200000088397115.jpg',
         'console' => 'PlayStation 4',
         'name' => 'Attack on Titan 2 - A.O.T. 2',
         'price' => '€ 59.99',
-    ],
-    'darksouls' => [
+    ),
+    array(
         'release' => '25-05-2018',
         'imageSRC' => 'images/9200000088311502.jpg',
         'console' => 'PlayStation 4',
         'name' => 'Dark Souls Remastered',
         'price' => '€ 59.99',
-    ],
-    'RDR2' => [
+    ),
+    array(
         'release' => '26-10-2018',
         'imageSRC' => 'images/9200000067180307.jpg',
         'console' => 'PlayStation 4',
         'name' => 'Red Dead Redemption 2',
         'price' => '<del>€ 49.99</del> € 39.99',
-    ],
-    'biomutant' => [
+    ),
+    array(
         'release' => '31-12-2018',
         'imageSRC' => 'images/9200000082972427.jpg',
         'console' => 'PlayStation 4',
         'name' => 'Biomutant',
         'price' => '€ 49.99',
-    ],
+    ),
 ];
 
 function makeCatalogue(array $products):string {
-    $sort = $_GET['sort'] ?? 'descending';
+    $sort = $_GET['sort'] ?? 'ascending';
     $amount = $_GET['amount'];
     echo $sort;
     $sort === 'ascending' ? sort($products) : rsort($products);
 
+    $names = array_column($products, 'name');
+    print_r($names);
+    array_multisort($names, $sort == 'ascending' ? SORT_DESC : SORT_ASC, $products );
+
     $fullCatalogue = '<section id="products">';
-    
     // somehow make the foreachloop only happen activate [$amount] times. - or find another loop to work with that also defines the nestled array variables -
     // for ($i=0; $i < $amount; $i++) { 
         foreach ($products as $games => $gameContent){
@@ -75,7 +78,7 @@ function makeCatalogue(array $products):string {
             $fullCatalogue .= '<span class="console">' .$gameContent['console']. '</span>';
             $fullCatalogue .= '<span class"name">' .$gameContent['name']. '</span>';
             $fullCatalogue .= '<span class="price">' .$gameContent['price']. '</span>';
-            $fullCatalogue .= '</article>' ;
+            $fullCatalogue .= '</article>';
         }
     // }
     $fullCatalogue .= '<section id="products">';
--- PHP-main/08-adresbalk/opdracht5/index.php
+++ PHP-main/08-adresbalk/opdracht5/index.php
@@ -1,3 +1,20 @@
+<?php
+	$panel1 = "";
+	$panel2 = "";
+	$panel3 = "";
+
+	if (isset($_GET['generate'])){
+		$rng1 = rand(1, 8); // for testing
+		$rng2 = rand(1, 8); // for testing
+		$rng3 = rand(1, 8); // for testing
+
+
+		$panel1 = 'images/panel-1-' .$rng1. '.png';
+		$panel2 = 'images/panel-2-' .$rng2. '.png';
+		$panel3 = 'images/panel-3-' .$rng3. '.png';
+
+	}
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -9,14 +26,31 @@
 		<div id="container">
 			<section class="comic">
 				<div class="panel-1">
-					<img src="images/panel-1-7.png" alt="panel 1" width="275" height="398">
+					<img src="<?=$panel1?>" alt="panel 1" width="275" height="398">
 				</div>
 				<div class="panel-2">
-					<img src="images/panel-2-5.png" alt="panel 2" width="275" height="398">
+					<img src="<?=$panel2?>" alt="panel 2" width="275" height="398">
 				</div>
 				<div class="panel-3">
-					<img src="images/panel-3-5.png" alt="panel 3" width="275" height="398">
+					<img src="<?=$panel3?>" alt="panel 3" width="275" height="398">
 				</div>
+			</section>
+			<section class="selection">
+				<form action="" method="GET">
+					<label for="panel1"> First panel</label>
+					<input type="number" name="panel1" id="panel1" min="0" max="8">
+					<label for="panel2"> Second panel</label>
+					<input type="number" name="panel2" id="panel2" min="0" max="8">
+					<label for="panel3"> Third panel</label>
+					<input type="number" name="panel3" id="panel3" min="0" max="8">
+					<br>
+					<input type="submit" name="generate" value="Generate">
+				</form>
+				<?php
+				echo $panel1. '<br>';
+				echo $panel2. '<br>';
+				echo $panel3. '<br>';
+				?>
 			</section>
 		</div>
 	</body>
