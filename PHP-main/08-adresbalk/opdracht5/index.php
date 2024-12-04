cd547af7c479d85b16dde74d80129e213cd6c379
OneAbusedBagel <Mikahaukkala@hotmail.com>
2024-12-04 15:29:40 +0100

    PHP 29ste les (TW)

--- PHP-main/08-adresbalk/opdracht5/index.php
+++ PHP-main/08-adresbalk/opdracht5/index.php
@@ -4,14 +4,26 @@
 	$panel3 = "";
 
 	if (isset($_GET['generate'])){
-		$rng1 = rand(1, 8); // for testing
-		$rng2 = rand(1, 8); // for testing
-		$rng3 = rand(1, 8); // for testing
+		$nmbr1 = $_GET['panel1'];
+		$nmbr2 = $_GET['panel2'];
+		$nmbr3 = $_GET['panel3'];
+		
+		
+		$panel1 = 'images/panel-1-' .$nmbr1. '.png';
+		$panel2 = 'images/panel-2-' .$nmbr2. '.png';
+		$panel3 = 'images/panel-3-' .$nmbr3. '.png';
+		
+	}
+	if (isset($_GET['random'])){
+		$nmbr1 = rand(1, 8);
+		$nmbr2 = rand(1, 8);
+		$nmbr3 = rand(1, 8);
+		
 
 
-		$panel1 = 'images/panel-1-' .$rng1. '.png';
-		$panel2 = 'images/panel-2-' .$rng2. '.png';
-		$panel3 = 'images/panel-3-' .$rng3. '.png';
+		$panel1 = 'images/panel-1-' .$nmbr1. '.png';
+		$panel2 = 'images/panel-2-' .$nmbr2. '.png';
+		$panel3 = 'images/panel-3-' .$nmbr3. '.png';
 
 	}
 ?>
@@ -38,19 +50,15 @@
 			<section class="selection">
 				<form action="" method="GET">
 					<label for="panel1"> First panel</label>
-					<input type="number" name="panel1" id="panel1" min="0" max="8">
+					<input type="number" name="panel1" id="panel1" min="1" max="8" value="<?=$nmbr1?>">
 					<label for="panel2"> Second panel</label>
-					<input type="number" name="panel2" id="panel2" min="0" max="8">
+					<input type="number" name="panel2" id="panel2" min="1" max="8" value="<?=$nmbr2?>">
 					<label for="panel3"> Third panel</label>
-					<input type="number" name="panel3" id="panel3" min="0" max="8">
+					<input type="number" name="panel3" id="panel3" min="1" max="8" value="<?=$nmbr3?>">
 					<br>
 					<input type="submit" name="generate" value="Generate">
+					<input type="submit" name="random" value="Random">
 				</form>
-				<?php
-				echo $panel1. '<br>';
-				echo $panel2. '<br>';
-				echo $panel3. '<br>';
-				?>
 			</section>
 		</div>
 	</body>
